a4f1a4b8a58044b63c22cc84211b2d4f22107449
Steve Raiff <sraiff@microplace.com>
2013-08-09 15:52:40 -0700

    updated net http

--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -5,11 +5,18 @@ class ProxyController < ApplicationController
   def default
     #send_data 1, disposition: 'inline'
     result = Net::HTTP.get_response("0.0.0.0", request.env["REQUEST_URI"], 3000)
-    Rails.logger.info "result.code = #{result.code}"
-    if result.code == 302 or result.code == 301
-      result = Net::HTTP.get_response(URI.parse(result.header['location']))
-    end
+    Rails.logger.info "result.headers = #{result.headers.to_s}"
+    result = filter_responses(result)
     @body = result.body
     render layout: false
+  end
+
+  def filter_responses(result)
+    if result.code == 302 or result.code == 301
+      result = Net::HTTP.get_response(URI.parse(result.header['location']))
+    elsif result.body == '<html><body>You are being <a href="http://0.0.0.0:3000/user/login">redirected</a>.</body></html>'
+       result = Net::HTTP.get_response("0.0.0.0", "/user/login", 3000)
+    end
+    result
   end
 end
