a47a6e8c3ec4d661051cb8e5c1937173d9d36488
Steve Raiff <sraiff@microplace.com>
2013-08-09 14:38:04 -0700

    fixed public/* files.  STarted handling 301,302's - poorly

--- README.md
+++ README.md
@@ -2,3 +2,11 @@ R4_interposer
 =============
 
 A Rails4 repo to explore an interposing controller to Rails2
+
+Setup:
+    An R2 version of Microplace under ../$r2_microplace
+    ln -s ../$r2_microplace/public public
+
+Operation:
+    [cd ../$r2_microplace ; script/server ]
+    rails s -p 3001
--- app/controllers/proxy_controller.rb
+++ app/controllers/proxy_controller.rb
@@ -3,9 +3,12 @@ class ProxyController < ApplicationController
   require "net/http"
 
   def default
-    Rails.logger.info "Here I am"
     #send_data 1, disposition: 'inline'
     result = Net::HTTP.get_response("0.0.0.0", request.env["REQUEST_URI"], 3000)
+    Rails.logger.info "result.code = #{result.code}"
+    if result.code == 302 or result.code == 301
+      result = Net::HTTP.get_response(URI.parse(result.header['location']))
+    end
     @body = result.body
     render layout: false
   end
